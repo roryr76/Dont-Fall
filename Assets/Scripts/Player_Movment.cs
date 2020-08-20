f80e54fb51de76802554c702f0347bcda2d42d8a
Owen000117 <owenodea@outlook.com>
2020-08-20 17:12:27 +0100

    Adding Player
    - Added in player movement.
    - Added a script to move the player.
    - Added a script for the camera to mover with the mouse.

---
+++ Assets/Scripts/Player_Movment.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Owen O' Dea
+ * K00218956
+ * 
+ * */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movment : MonoBehaviour
+{
+
+    public CharacterController controller;
+    // Sets the player movment and the gravity.
+    public float speed = 15.0f;
+    public float gravity = -9.81f;
+    //gets the location of groundcheck
+    public Transform groundCheck;
+    //sets the distance the player wants to stay away from the ground
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isOnGround;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // checks to see if the player is on the ground
+        isOnGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        // If the player is not o the ground the player is moved down on the y axes. 
+        if (isOnGround && velocity.y < 0)
+        {
+            velocity.y = -2.0f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //Takes input and moves the player 
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        //Moves the player by time instead of frames.  
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
